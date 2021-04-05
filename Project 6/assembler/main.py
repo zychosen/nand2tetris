e0244ab1011d8941baab9aac8b65fb6dc61f8df2
zychosen <63509173+zychosen@users.noreply.github.com>
2021-04-05 22:09:58 +0530

    Add files via upload

--- Project 6/assembler/main.py
+++ Project 6/assembler/main.py
@@ -2,16 +2,10 @@ import sys
 import Handler
 
 def main():
-	# Command line arguments for input and output files
-	fileObject = open(sys.argv[1], 'r')
-	saveObject = open(sys.argv[2], 'w')
 
-	labels = Handler.save_labels(fileObject)  # Obtaining list of labels in asm file
-	fileObject.seek(0)    # reset file object read pointer
-	Handler.handle(fileObject, saveObject, labels)
+	with open(sys.argv[1], 'r') as fileObject, open(sys.argv[2], 'w') as saveObject:
+		labels = Handler.save_labels(fileObject)
+		fileObject.seek(0)   
+		Handler.handle(fileObject, saveObject, labels)
 
-	# Closing file objects
-	fileObject.close()
-	saveObject.close()
-
-main() # Calling main function
+main()
